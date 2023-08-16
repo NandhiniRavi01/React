fa7d5b9088f91350966543531bf3c270d37336af
NandhiniRavi27 <113678413+NandhiniRavi27@users.noreply.github.com>
2023-08-16 13:09:15 +0530

    Add files via upload

---
+++ src/Component/Company.js
@@ -0,0 +1,8 @@
+const Company= () => {
+    return(
+        <div>
+        <h1>Company</h1>
+        </div>
+    );
+}
+export default Company;
---
+++ src/Component/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div>
+            <h2>Customer Data Management
+App using Next.js and React</h2>
+        </div>
+     );
+}
+ 
+export default Home;
